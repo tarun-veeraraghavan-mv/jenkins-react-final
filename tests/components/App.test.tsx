c75a52cbb256f62105065e681dda934e68a7d9fc
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-05-25 10:23:48 -0700

    Initial commit

---
+++ tests/components/App.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import "@testing-library/jest-dom";
+
+import App from "../../src/App";
+
+describe("App", () => {
+  it("should render 'Hello World'", () => {
+    render(<App />);
+
+    const text = screen.getByText(/hello world/i);
+    expect(text).toBeInTheDocument();
+  });
+});
