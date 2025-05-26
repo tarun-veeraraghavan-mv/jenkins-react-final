3a4dd82a71da7b1981a5264807147c110678353b
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-05-25 18:30:59 -0700

    fix unit and e2e test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: { environment: "jsdom", globals: true, setupFiles: "tests/setup.ts" },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "tests/setup.ts",
+    exclude: [
+      "**/node_modules/**",
+      "**/e2e/**",
+      "**/*.e2e.*",
+      "**/*.spec.ts", // if your e2e tests use .spec.ts
+    ],
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
 });
